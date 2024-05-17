be25afdea8baa0554871d49966fb305b87d83c5c
Marcio Bera <marciobera@hotmail.com>
2024-05-17 21:02:01 +0100

    chore: add initial projet config

---
+++ create.sql
@@ -0,0 +1,13 @@
+drop schema ccca cascade;
+
+create schema ccca;
+
+create table ccca.account (
+	account_id uuid primary key,
+	name text not null,
+	email text not null,
+	cpf text not null,
+	car_plate text null,
+	is_passenger boolean not null default false,
+	is_driver boolean not null default false
+);
