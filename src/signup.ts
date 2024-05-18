f8db8408e87632733abf46e3db6e3968a5b90e24
Marcio Bera <marcio.bera@bauermedia.co.uk>
2024-05-18 13:17:39 +0100

    refactor: making signup more readable

--- src/signup.ts
+++ src/signup.ts
@@ -6,53 +6,17 @@ export async function signup(input: any): Promise<any> {
     const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
     try {
         const id = crypto.randomUUID();
-
-        const [acc] = await connection.query("select * from ccca.account where email = $1", [input.email]);
-        if (!acc) {
-
-            if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-                if (input.email.match(/^(.+)@(.+)$/)) {
-
-                    if (validateCpf(input.cpf)) {
-                        if (input.isDriver) {
-                            if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-                                await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-                                const obj = {
-                                    accountId: id
-                                };
-                                return obj;
-                            } else {
-                                // invalid car plate
-                                return -5;
-                            }
-                        } else {
-                            await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-
-                            const obj = {
-                                accountId: id
-                            };
-                            return obj;
-                        }
-                    } else {
-                        // invalid cpf
-                        return -1;
-                    }
-                } else {
-                    // invalid email
-                    return -2;
-                }
-
-            } else {
-                // invalid name
-                return -3;
-            }
-
-        } else {
-            // already exists
-            return -4;
-        }
-
+        const [existingAccount] = await connection.query("select * from ccca.account where email = $1", [input.email]);
+        if (existingAccount) return -4;
+        if (!input.name.match(/[a-zA-Z] [a-zA-Z]+/)) return -3;
+        if (!input.email.match(/^(.+)@(.+)$/)) return -2;
+        if (!validateCpf(input.cpf)) return -1;
+        if (input.isDriver && !input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) return -5;
+        await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
+        const output = {
+            accountId: id
+        };
+        return output;
     } finally {
         await connection.$pool.end();
     }
