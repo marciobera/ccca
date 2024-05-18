1fdee88f7a52983db1fb8629056cae11efbff3ab
Marcio Bera <marcio.bera@bauermedia.co.uk>
2024-05-18 12:30:56 +0100

    test: add tests to validate cpf

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,20 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each([
+    "97456321558",
+    "71428793860",
+    "87748248800"
+])("Should test if the CPF is valid: %s", (cpf: string) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(true);
+})
+
+test.each([
+    "97456321559",
+    null,
+    undefined,
+    "11111111111"
+])("Should test if the CPF is invalid: %s", (cpf: any) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toBe(false);
+})
